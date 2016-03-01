7bbf099072b08857905c3ae12e310d065c790d4d
Dmitry Alimov <dvalimov@gmail.com>
2016-03-01 11:53:34 +0300

    Added to_phonetic function

--- strs/strs.py
+++ strs/strs.py
@@ -49,3 +49,50 @@ def bits_to_str(bits):
     Convert a string of bits to string
     """
     return ''.join(chr(int(bits[i:i + 8], 2)) for i in xrange(0, len(bits), 8))
+
+
+def to_phonetic(text):
+    """
+    Convert a text to phonetic alphabet text
+    """
+    alp = {
+        # letters
+        'A': 'Alpha',
+        'B': 'Bravo',
+        'C': 'Charlie',
+        'D': 'Delta',
+        'E': 'Echo',
+        'F': 'Foxtrot',
+        'G': 'Golf',
+        'H': 'Hotel',
+        'I': 'India',
+        'J': 'Juliet',
+        'K': 'Kilo',
+        'L': 'Lima',
+        'M': 'Mike',
+        'N': 'November',
+        'O': 'Oscar',
+        'P': 'Papa',
+        'Q': 'Quebec',
+        'R': 'Romeo',
+        'S': 'Sierra',
+        'T': 'Tango',
+        'U': 'Uniform',
+        'V': 'Victor',
+        'W': 'Whiskey',
+        'X': 'X-ray',
+        'Y': 'Yankee',
+        'Z': 'Zulu',
+        # digits
+        '0': 'Zero',
+        '1': 'One',
+        '2': 'Two',
+        '3': 'Three',
+        '4': 'Four',
+        '5': 'Five',
+        '6': 'Six',
+        '7': 'Seven',
+        '8': 'Eight',
+        '9': 'Nine',
+    }
+    return ' '.join([alp.get(x, x) for x in ''.join(text.split()).upper()])
